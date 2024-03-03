a1cb94771f4c28605df0bf7b65ee5e8348ad6ecb
ihebchhibi <iheb.chhibi@gmail.com>
2024-03-03 21:52:42 +0100

    first commit

---
+++ src/components/Cell.vue
@@ -0,0 +1,9 @@
+<template>
+    <canvas class="cell" ref="cell" :width="50" :height="50"></canvas>
+  </template>
+  <script setup lang="ts"></script>
+  <style scoped lang="scss">
+  .cell {
+    border: 1px solid black;
+  }
+  </style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="grid">
+    <div v-for="n in 5" class="rows">
+      <Cell v-for="m in 5">({{ m }}, {{ n }})</Cell>
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import Cell from "./Cell.vue"
+</script>
+
+<style scoped lang="scss">
+.grid {
+  display: flex;
+  flex-direction: column;
+}
+.rows {
+  display: flex;
+  flex-direction: row;
+}
+</style>
