3c41067fd3a539de33e22a430077a206c49774fa
ihebchhibi <iheb.chhibi@gmail.com>
2024-03-24 16:21:39 +0100

    maybe last

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,9 +1,11 @@
 <template>
-    <canvas class="cell" ref="cell" :width="50" :height="50"></canvas>
+    <canvas class="cell" ref="cell" :width="5" :height="5"></canvas>
   </template>
   <script setup lang="ts"></script>
   <style scoped lang="scss">
   .cell {
-    border: 1px solid black;
+    border: 1px solid gray;
+    margin-left: -1px;
+    margin-bottom: -1px;
   }
   </style>
